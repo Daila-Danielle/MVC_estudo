2fd7767e16f4115dea397619e240697934269d80
Daila-Danielle <dailasantos565@gmail.com>
2023-01-16 21:34:25 -0300

    conexão

--- App/Models/PessoaModel.php
+++ App/Models/PessoaModel.php
@@ -1,11 +1,21 @@
 <?php
 
+/* define('HOST', '127.0.0.1');
+define('USER', 'root');
+define('PASS', '');
+define('BASE', 'treinamento_daila');
+
+$conexao = new MySQLi(HOST, USER, PASS, BASE,3308);
+if(!$conexao) {
+    echo 'Deu ruim parça!';
+}
+ */
 class PessoaModel
 {
     public $id, $nome, $data_nascimento;
 
     public function save()
     {
-        
+
     }
 }
