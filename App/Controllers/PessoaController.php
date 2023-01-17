0f5ae6519d38809f79f062a2b935462cf80f2108
Daila Santos <111146154+Daila-Danielle@users.noreply.github.com>
2023-01-17 14:39:53 -0300

    Update PessoaController.php

--- App/Controllers/PessoaController.php
+++ App/Controllers/PessoaController.php
@@ -2,19 +2,49 @@
 
 class PessoaController
 {
+    /**
+     * Método para exibir a lista de pessoas
+     * Inclui o arquivo com o modelo da pessoa e carrega a view de listagem
+     */
     public static function index()
     {
+        include 'Models/PessoaModel.php';
+
+        $model = new PessoaModel();
+        $model->getAllRows();
         include 'Views/modules/Pessoa/ListaPessoa.php';
+        
     }
 
+    /**
+     * Método para exibir o formulário de cadastro de pessoas
+     * Inclui o arquivo da view de formulário
+     */
     public static function form()
     {
         include 'Views/modules/Pessoa/FormPessoa.php';
     }
 
+    /**
+     * Método para salvar os dados enviados pelo formulário
+     * Inclui o arquivo com o modelo da pessoa, cria uma nova instância do modelo
+     * preenche os atributos com os dados do $_POST e chama o método de salvar
+     * redireciona o usuário para a página de listagem
+     */
     public static function save()
     {
-        var_dump($_POST);
+        include 'Models/PessoaModel.php';
+
+        $model = new PessoaModel();
+
+        $model->nome = $_POST['nome'];
+        $model->cpf = $_POST['cpf'];
+        $model->data_nascimento = $_POST['data_nascimento'];
+
+        $model ->save();
+        
+        header("Location: /treinamento/Daila/php/MVC_estudo/pessoa");
+        
     }
 
 }
