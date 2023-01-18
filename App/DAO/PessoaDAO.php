2f951eb89733996609ee2f01d8749212decf34ff
Daila-Danielle <dailasantos565@gmail.com>
2023-01-17 21:09:12 -0300

    atualização

--- App/DAO/PessoaDAO.php
+++ App/DAO/PessoaDAO.php
@@ -9,12 +9,14 @@ class PessoaDAO
     {
        
 
-        define('HOST', '192.168.1.254');
+/*         define('HOST', '192.168.1.254');
         define('USER', 'estagiario');
         define('PASS', 'estagio123');
         define('BASE', 'treinamento_daila');
       
-        $this->conexao = new MySQLi(HOST, USER, PASS, BASE);
+        $this->conexao = new MySQLi(HOST, USER, PASS, BASE); */
+
+        $this->conexao = new MySQLi('127.0.0.1', 'root', '', 'test');
         
 
     }
