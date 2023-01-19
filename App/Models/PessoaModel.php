bdb5f6cfb6cb19bbfc2adb6d349803d216905e60
Daila Santos <111146154+Daila-Danielle@users.noreply.github.com>
2023-01-19 14:57:20 -0300

    Update PessoaModel.php

--- App/Models/PessoaModel.php
+++ App/Models/PessoaModel.php
@@ -1,21 +1,54 @@
 <?php
+
+namespace App\Models;
+
+use App\DAO\PessoaDAO;
 class PessoaModel
 {
     public $id, $nome, $cpf, $data_nascimento;
     public $rows;
     public function save()
     {
-        include 'DAO/PessoaDAO.php';
+        
         $dao = new PessoaDAO();
-        $dao->insert($this);
+     
+        if(empty($this->id))
+        {
+            $dao->insert($this);
+
+        } else {
+
+            $dao->update($this);
+            
+        }
+
     }
 
     public function getAllRows()
     {
-        include 'DAO/PessoaDAO.php';
+        
         $dao = new PessoaDAO();
         $this->rows = $dao->select();
         
+    }
+
+    public function getById(int $id)
+    {
+        
+        $dao = new PessoaDAO();
+
+        $obj = $dao->selectById($id);
+
+        return ($obj) ? $obj : new PessoaModel;
+
+    }
+
+    public function delete(int $id)
+    {
+        
+        $dao = new PessoaDAO();
+        $dao->delete($id);
+
     }
 
 }
