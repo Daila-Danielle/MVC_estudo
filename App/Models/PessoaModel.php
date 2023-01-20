0930433574c692adf2c8c7ba6a2e0daab38d72a8
Daila Santos <111146154+Daila-Danielle@users.noreply.github.com>
2023-01-20 14:12:24 -0300

    Update PessoaModel.php

--- App/Models/PessoaModel.php
+++ App/Models/PessoaModel.php
@@ -1,73 +1,54 @@
 <?php
 
-namespace App\DAO;
+namespace App\Models;
 
-Use App\Models\PessoaModel;
-class PessoaDAO
+use App\DAO\PessoaDAO;
+class PessoaModel extends Model
 {
-    private $conexao;
-
-
-    public function __construct()
-    {
-       
-
-        define('HOST', '192.168.1.254');
-        define('USER', 'estagiario');
-        define('PASS', 'estagio123');
-        define('BASE', 'treinamento_daila');
-      
-        $this->conexao = new \MySQLi(HOST, USER, PASS, BASE);
-        
-
-    }
-
-    public function insert(PessoaModel $model)
-    {
-        $sql = "INSERT INTO tbl_pessoa (nome, cpf, data_nascimento)
-        VALUES ('{$model->nome}','{$model->cpf}','{$model->data_nascimento}')";
-        $res  = $this->conexao->query($sql);
-
-    }
-
-    public function update(PessoaModel $model)
-    {
-        $sql = "UPDATE  tbl_pessoa SET nome = '{$model->nome}', cpf = '{$model->cpf}', data_nascimento = $model->data_nascimento
-        WHERE id = $model->id";
-        $res  = $this->conexao->query($sql);
-
-        return $res;
-       
-    }
-
-    public function select()
-    {
-        $sql = "SELECT * FROM tbl_pessoa";
-        $res = $this->conexao->query($sql);
-        $results = array();
-        
-        while ($row = $res->fetch_object()) {
-            $results[] = $row;
-        }
-        
-        return $results;
-    }
-
-    public function selectById(int $id)
-    {
-        
-        $sql = "SELECT * FROM tbl_pessoa WHERE id = $id";
-        $res = $this->conexao->query($sql);
-        
-        return $res->fetch_object("App\Model\PessoaModel");
-    }
+    public $id, $nome, $cpf, $data_nascimento;
     
+    public function save()
+    {
+        
+        $dao = new PessoaDAO();
+     
+        if(empty($this->id))
+        {
+            $dao->insert($this);
+
+        } else {
+
+            $dao->update($this);
+            
+        }
+
+    }
+
+    public function getAllRows()
+    {
+        
+        $dao = new PessoaDAO();
+        $this->rows = $dao->select();
+        
+    }
+
+    public function getById(int $id)
+    {
+        
+        $dao = new PessoaDAO();
+
+        $obj = $dao->selectById($id);
+
+        return ($obj) ? $obj : new PessoaModel;
+
+    }
 
     public function delete(int $id)
     {
-        $sql = "DELETE FROM tbl_pessoa WHERE id  = $id";
-        $res  = $this->conexao->query($sql);
-    }
+        
+        $dao = new PessoaDAO();
+        $dao->delete($id);
 
+    }
 
 }
