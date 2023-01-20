33ad73ee0b7f3646aed2b3f05a452d86070a5499
Daila Santos <111146154+Daila-Danielle@users.noreply.github.com>
2023-01-20 14:11:54 -0300

    Update PessoaDAO.php

--- App/DAO/PessoaDAO.php
+++ App/DAO/PessoaDAO.php
@@ -1,23 +1,17 @@
 <?php
 
-class PessoaDAO
+namespace App\DAO;
+
+Use App\Models\PessoaModel;
+class PessoaDAO extends DAO
 {
     private $conexao;
 
 
     public function __construct()
     {
-       
 
-/*         define('HOST', '192.168.1.254');
-        define('USER', 'estagiario');
-        define('PASS', 'estagio123');
-        define('BASE', 'treinamento_daila');
-      
-        $this->conexao = new MySQLi(HOST, USER, PASS, BASE); */
-
-        $this->conexao = new MySQLi('127.0.0.1', 'root', '', 'test');
-        
+        parent::__construct();
 
     }
 
@@ -31,7 +25,12 @@ class PessoaDAO
 
     public function update(PessoaModel $model)
     {
-        
+        $sql = "UPDATE  tbl_pessoa SET nome = '{$model->nome}', cpf = '{$model->cpf}', data_nascimento = $model->data_nascimento
+        WHERE id = $model->id";
+        $res  = $this->conexao->query($sql);
+
+        return $res;
+       
     }
 
     public function select()
@@ -47,9 +46,20 @@ class PessoaDAO
         return $results;
     }
 
-    public function delete(PessoaModel $model)
+    public function selectById(int $id)
     {
         
+        $sql = "SELECT * FROM tbl_pessoa WHERE id = $id";
+        $res = $this->conexao->query($sql);
+        
+        return $res->fetch_object("App\Model\PessoaModel");
+    }
+    
+
+    public function delete(int $id)
+    {
+        $sql = "DELETE FROM tbl_pessoa WHERE id  = $id";
+        $res  = $this->conexao->query($sql);
     }
 
 
